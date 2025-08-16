3d89ceb001106b8a5c8e23b6a4617a47c57c0112
19ty84 <2647748131@qq.com>
2025-08-17 02:41:57 +0800

    changed music preview

--- scripts/music-preview.js
+++ scripts/music-preview.js
@@ -1,22 +1,18 @@
 document.querySelectorAll('details.music-player').forEach(item => {
-    item.addEventListener('toggle', function () {
-        if (this.open) { // 仅当展开时加载
-            const container = this.querySelector('.player-container');
-            const playerUrl = container.dataset.src;
-            container.innerHTML = `
-              <iframe 
-                frameborder="no"
-                border="0"
-                marginwidth="0"
-                marginheight="0"
-                width=298
-                height=52
-                src="${playerUrl}" 
-              ></iframe>
-            `;
-        }
-        if (!this.open) { // 销毁
-            this.querySelector('.player-container').innerHTML = '';
-        }
-    });
+  item.addEventListener('toggle', function () {
+    if (this.open) { // 仅当展开时加载
+      const container = this.querySelector('.player-container');
+      const playerUrl = container.dataset.src;
+      container.innerHTML = `
+        <audio controls="controls" preload="none">
+          <source src="${playerUrl}" type="audio/wav">
+          <embed height="100" width="100" src="${playerUrl}" />
+          您的浏览器不支持该 audio 预览。
+        </audio>
+      `;
+    }
+    if (!this.open) { // 销毁
+      this.querySelector('.player-container').innerHTML = '';
+    }
+  });
 });
