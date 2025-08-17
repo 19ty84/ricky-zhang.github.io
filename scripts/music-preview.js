1560dd81c5f364edcf5e3853a0ff6cfe0a7a8ad8
19ty84 <2647748131@qq.com>
2025-08-17 18:53:13 +0800

    added header and footer

--- scripts/music-preview.js
+++ scripts/music-preview.js
@@ -4,7 +4,7 @@ document.querySelectorAll('details.music-player').forEach(item => {
       const container = this.querySelector('.player-container');
       const playerUrl = container.dataset.src;
       container.innerHTML = `
-        <audio controls preload="none">
+        <audio controls preload="auto">
           <source src="${playerUrl}" type="audio/wav">
           您的浏览器不支持该 audio 预览。
         </audio>
